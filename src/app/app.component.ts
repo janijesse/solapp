3198d29e44ca4f2f28a9276a5ffe5e8c6473ad88
janijesse <jani@Mac-mini-de-Jani.local>
2024-02-15 12:28:37 +0100

    Mostrar saldo Token Silly

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,25 +1,57 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
 import { RouterModule } from '@angular/router';
 import { HdWalletMultiButtonComponent } from '@heavy-duty/wallet-adapter-material';
-
+import { ShyftApiService } from './shyft.api.service';
+import { WalletStore } from '@heavy-duty/wallet-adapter';
+import { toSignal } from '@angular/core/rxjs-interop'
+import { computedAsync } from 'ngxtension/computed-async';
+import { DecimalPipe } from '@angular/common';
 @Component({
   standalone: true,
   imports: [
     RouterModule,
-    HdWalletMultiButtonComponent
+    HdWalletMultiButtonComponent,
   ],
   selector: 'solapp-root',
   template: `
-    <header class="py-8">
+    <header class="px-16 pt-20 pb-8 relative">
         <h1 class="text-5xl text-center mb-4">Solapp</h1>
-        <div class="flex justify-center">
-        <hd-wallet-multi-button></hd-wallet-multi-button>
+        <div class="flex justify-center mb-4">
+          <hd-wallet-multi-button></hd-wallet-multi-button>
         </div>
        
+        @if (account()){
+          <p class="flex-justify-center text-center mb-4">Welcome</p>
+          <div class="absolute top-4 left-4 flex justify-center items-center gap-2 ">
+            
+            <img [src]="account()?.info?.image" class="w-8 h-8">
+            <p class="text-xl font-bold">
+              {{ account()?.balance }}</p>
+          </div>
+          
+        }
+
+        
+       
     </header>
+   
     `,
   
 })
 export class AppComponent {
+ private readonly _shyftApiService = inject(ShyftApiService);
+ private readonly _walletStore = inject(WalletStore);
+ private readonly _publicKey = toSignal(this._walletStore.publicKey$);
+
+ readonly account = computedAsync(
+  () => this._shyftApiService.getAccount(this._publicKey()?.toBase58()),
+  { requireSync: true },
+ );
   
+
+
 }
+
+
+
+
